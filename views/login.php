c31a391690450c4802c8163eee957d7f1a160b39
Việt <chrisvietvu@gmail.com>
2025-11-23 04:45:36 +0700

    Add files via upload

--- views/login.php
+++ views/login.php
@@ -1,4 +1,35 @@
-<div class="row justify-content-center">
+<style>
+    /* Lớp nền hình ảnh */
+    .login-background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1; /* Đẩy hình nền ra sau cùng */
+        
+        /* Link hình ảnh siêu thị (bạn có thể thay đổi link này) */
+        background-image: url('https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQVMglOAfPmkqvO90DPpOhQ8B4en1MBkzK6jXw1XbhTH6ZNfw54-n9RthajKUrLFnR7yCJxAKp1isjVgKJb3m4SQQLKSClydmoy7RHB4v8dJZBXv_M');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+
+        /* Hiệu ứng làm mờ và làm tối ảnh để form nổi bật hơn */
+        filter: blur(1px) brightness(0.4); 
+        -webkit-filter: blur(1px) brightness(0.4);
+    }
+
+    /* Đảm bảo card nằm giữa màn hình đẹp hơn */
+    .login-container {
+        min-height: 80vh; /* Chiều cao tối thiểu để căn giữa dọc */
+        display: flex;
+        align-items: center;
+    }
+</style>
+
+<div class="login-background"></div>
+
+<div class="row justify-content-center login-container">
     <div class="col-md-6 col-lg-4">
         <div class="card shadow-lg border-0 border-top border-primary border-5 rounded-3">
             <div class="card-header bg-primary text-white text-center py-3 rounded-top-3">
